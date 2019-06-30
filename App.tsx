3fe8e8ad9aa8504cb5cf2f01c13f42251c3abde2
Sasha Prokhorenko <djminikin@gmail.com>
2019-06-30 16:31:27 +0200

    Minor update

--- App.tsx
+++ App.tsx
@@ -2,35 +2,43 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as data from './src/assets/off-pistes.json';
+import { OffPiste } from './src/models/OffPiste.js';
 
 interface Props {}
 export default class App extends Component<Props> {
-	render() {
-		console.log(data);
-		const json = JSON.stringify(data);
-		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>{json}</Text>
-			</View>
-		);
-	}
+  render() {
+    const json = JSON.stringify(data);
+    const posts = JSON.parse(json) as [OffPiste];
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{posts.toString}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
 });
+
+const logPost = (id: number) => {
+  console.log(`
+    Id: ${id}
+  `);
+};
